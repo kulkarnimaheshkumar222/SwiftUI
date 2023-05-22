cbce6e9a337e75f52b09734c2c0779d8127e3fe0
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2023-05-22 06:31:27 +0530

    load remote image

---
+++ Project1-LoadRemoteImage/Project1-LoadRemoteImage/Project1_LoadRemoteImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project1_LoadRemoteImageApp.swift
+//  Project1-LoadRemoteImage
+//
+//  Created by Mahesh Kulkarni on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Project1_LoadRemoteImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
