ccd33bd5f1f6f6254ecab1f484edb97778dedd7b
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2023-12-25 23:19:12 +0530

    Initial set up for AppleFramework app complete

---
+++ Project4-AppleFrameworkApp/Project4-AppleFrameworkApp/Project4_AppleFrameworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project4_AppleFrameworkAppApp.swift
+//  Project4-AppleFrameworkApp
+//
+//  Created by Mahesh Kulkarni on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Project4_AppleFrameworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
